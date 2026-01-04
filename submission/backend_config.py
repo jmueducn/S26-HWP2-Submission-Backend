362ac3dee11253b4110c4fd9afce6f24149812f8
Puru Samal <puruboii@gmail.com>
2026-01-04 12:49:16 -0600

    config file fix...

--- submission/backend_config.py
+++ submission/backend_config.py
@@ -129,19 +129,9 @@ class BackendConfig:
 # ------------------------------------------------------------------
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(FILE_PATH, "configs/hw1p2.json"), "r", encoding="utf-8") as f:
-    config_data = json.load(f)
-HW1P2_BACKEND_CONFIG = BackendConfig.from_dict(config_data)
+# TODO: HW1P2_BACKEND_CONFIG, HW3P2_BACKEND_CONFIG, HW4P2_BACKEND_CONFIG
 
 with open(os.path.join(FILE_PATH, "configs/hw2p2.json"), "r", encoding="utf-8") as f:
     config_data = json.load(f)
 HW2P2_BACKEND_CONFIG = BackendConfig.from_dict(config_data)
-
-with open(os.path.join(FILE_PATH, "configs/hw3p2.json"), "r", encoding="utf-8") as f:
-    config_data = json.load(f)
-HW3P2_BACKEND_CONFIG = BackendConfig.from_dict(config_data)
-
-with open(os.path.join(FILE_PATH, "configs/hw4p2.json"), "r", encoding="utf-8") as f:
-    config_data = json.load(f)
-HW4P2_BACKEND_CONFIG = BackendConfig.from_dict(config_data)
 
